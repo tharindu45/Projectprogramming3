52c42f3cbc7bebcc57a26a2771eefb78a2667b1c
tharindu45 <prasadtharindu816@gmail.com>
2020-09-18 08:05:19 -0700

    fill set and get method

--- Example.java
+++ Example.java
@@ -7,28 +7,28 @@ public class CscExample
 	
 	public void  setQuectionNo()
 	{
-          	
+        this.quectionNo=QuectionNo;		  	
 	}
 	public void  setCategory()
 	{
-		
+		this.category=category;
 	} 
 	public void setText()
 	{
-		
+		this.text=text;
 	}
 	public int getQuectionNo()
 	{
-		
+		return quectionNo;
 	}
 	public  String getCategory()
 	{
-		
+		return category;
 	}
 	public String getText()
 	{
 		
-		
+		return text;
 	}
 	
  
